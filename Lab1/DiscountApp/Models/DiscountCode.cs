27d5b465ef96ce54ab51bc87f0e888b8190be78c
Fabian Barański <bisik@vp.pl>
2026-01-12 19:03:44 +0100

    Initial Commit.

---
+++ Lab1/DiscountApp/Models/DiscountCode.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab1.Models;
+
+public abstract class DiscountCode
+{
+    private int Id { get; set; }
+    private string Code { get; set; }
+    private CodeStatus Status { get; set; }
+}
--- Lab1/DiscountApp/Program.cs
+++ Lab1/DiscountApp/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
-using Lab1.Services;
+
 
 Console.WriteLine("CAR SHOP!");
-ProgramService.StartInstance();
+
+
+return 0;
 
 
     
